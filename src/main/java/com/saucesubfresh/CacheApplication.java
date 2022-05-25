6853b9e0b4b0c2e3e37ae34d99e0d6b534d1c168
lijunping <2544054976@qq.com>
2022-05-26 07:53:11 +0800

    feat:测试

--- src/main/java/com/saucesubfresh/CacheApplication.java
+++ src/main/java/com/saucesubfresh/CacheApplication.java
@@ -1,13 +1,15 @@
 package com.saucesubfresh;
 
+import com.saucesubfresh.starter.cache.annotation.EnableCache;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * @author: 李俊平
  * @Date: 2022-05-21 09:16
  */
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class CacheApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/saucesubfresh/service/impl/AsyncUserServiceImpl.java
+++ src/main/java/com/saucesubfresh/service/impl/AsyncUserServiceImpl.java
@@ -1,29 +1,29 @@
-package com.saucesubfresh.service.impl;
-
-import com.github.benmanes.caffeine.cache.AsyncCache;
-import com.github.benmanes.caffeine.cache.Caffeine;
-import com.saucesubfresh.entity.UserEntity;
-import com.saucesubfresh.service.UserService;
-import org.springframework.stereotype.Service;
-
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-
-/**
- * @author: 李俊平
- * @Date: 2022-05-22 20:50
- */
-@Service
-public class AsyncUserServiceImpl implements UserService {
-
-
-    // 使用executor设置线程池
-    AsyncCache<Integer, Integer> asyncCache = Caffeine.newBuilder()
-            .expireAfterWrite(1, TimeUnit.MINUTES)
-            .maximumSize(100).buildAsync();
-
-    @Override
-    public UserEntity loadUser(Integer id) {
-        return null;
-    }
-}
+//package com.saucesubfresh.service.impl;
+//
+//import com.github.benmanes.caffeine.cache.AsyncCache;
+//import com.github.benmanes.caffeine.cache.Caffeine;
+//import com.saucesubfresh.entity.UserEntity;
+//import com.saucesubfresh.service.UserService;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.concurrent.Executors;
+//import java.util.concurrent.TimeUnit;
+//
+///**
+// * @author: 李俊平
+// * @Date: 2022-05-22 20:50
+// */
+//@Service
+//public class AsyncUserServiceImpl implements UserService {
+//
+//
+//    // 使用executor设置线程池
+//    AsyncCache<Integer, Integer> asyncCache = Caffeine.newBuilder()
+//            .expireAfterWrite(1, TimeUnit.MINUTES)
+//            .maximumSize(100).buildAsync();
+//
+//    @Override
+//    public UserEntity loadUser(Integer id) {
+//        return null;
+//    }
+//}
