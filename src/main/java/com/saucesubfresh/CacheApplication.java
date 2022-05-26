f4ce1d65ef49aacc7d0f76cecb9a371b0de92858
李俊平 <2544054976@qq.com>
2022-05-26 15:25:14 +0800

    feat:测试缓存

--- src/main/java/com/saucesubfresh/CacheApplication.java
+++ src/main/java/com/saucesubfresh/CacheApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
  * @author: 李俊平
  * @Date: 2022-05-21 09:16
  */
+@EnableCache
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class CacheApplication {
 
