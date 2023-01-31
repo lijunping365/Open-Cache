ff478da87934e4b9b4a90ad49b9bd3fdf9266dec
lijunping <2544054976@qq.com>
2023-01-31 20:40:32 +0800

    :art: 优化

--- cache-sample/src/main/java/com/saucesubfresh/cache/sample/component/metrics/ICacheMetricsPusher.java
+++ cache-sample/src/main/java/com/saucesubfresh/cache/sample/component/metrics/ICacheMetricsPusher.java
@@ -1,6 +1,10 @@
 package com.saucesubfresh.cache.sample.component.metrics;
 
+import com.saucesubfresh.starter.cache.metrics.CacheMetrics;
+import com.saucesubfresh.starter.cache.metrics.CacheMetricsPusher;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 /**
  * 定时上报缓存情况，为了生成每日报表
@@ -8,7 +12,10 @@ import org.springframework.stereotype.Component;
  * @author lijunping on 2022/6/24
  */
 @Component
-public class ICacheMetricsPusher{
+public class ICacheMetricsPusher implements CacheMetricsPusher {
 
+    @Override
+    public void pushCacheMetrics(List<CacheMetrics> list) {
 
+    }
 }
